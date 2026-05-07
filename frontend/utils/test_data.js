06695d4c75a40c1ed301e65611a7899138e1d02b
Waseem Basit <wbasit.ai@gmail.com>
2026-05-07 18:25:05 -0500

    Add files via upload

---
+++ frontend/utils/test_data.js
@@ -0,0 +1,114 @@
+export const detections = [
+    {
+        "profileImage": "image",
+        "spO2": 96,
+        "bp": 97,
+        "bpm": 107,
+        "temp": 37.5,
+        "prediction": "Med"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 92,
+        "bp": 116,
+        "bpm": 151,
+        "temp": 38.5,
+        "prediction": "High"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 91,
+        "bp": 79,
+        "bpm": 135,
+        "temp": 38.4,
+        "prediction": "High"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 96,
+        "bp": 95,
+        "bpm": 92,
+        "temp": 37.3,
+        "prediction": "Med"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 96,
+        "bp": 97,
+        "bpm": 97,
+        "temp": 37.4,
+        "prediction": "Low"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 91,
+        "bp": 121,
+        "bpm": 98,
+        "temp": 37.8,
+        "prediction": "Med"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 90,
+        "bp": 116,
+        "bpm": 96,
+        "temp": 38.4,
+        "prediction": "Med"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 90,
+        "bp": 118,
+        "bpm": 107,
+        "temp": 39.0,
+        "prediction": "Med"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 88,
+        "bp": 87,
+        "bpm": 107,
+        "temp": 40.7,
+        "prediction": "High"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 98,
+        "bp": 127,
+        "bpm": 72,
+        "temp": 36.6,
+        "prediction": "Low"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 80,
+        "bp": 91,
+        "bpm": 131,
+        "temp": 38.6,
+        "prediction": "High"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 97,
+        "bp": 138,
+        "bpm": 77,
+        "temp": 36.7,
+        "prediction": "Low"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 96,
+        "bp": 111,
+        "bpm": 105,
+        "temp": 38.5,
+        "prediction": "Med"
+    },
+    {
+        "profileImage": "image",
+        "spO2": 88,
+        "bp": 94,
+        "bpm": 131,
+        "temp": 39.3,
+        "prediction": "High"
+    },
+]
