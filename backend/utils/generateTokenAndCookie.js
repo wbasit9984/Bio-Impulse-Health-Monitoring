fe4e0a3f5d7166f94d715ad6943156070cc10693
Waseem Basit <wbasit.ai@gmail.com>
2026-05-07 19:05:34 -0500

    Add files via upload

---
+++ backend/utils/generateTokenAndCookie.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const generateTokenAndCookie = (userID, res) => {
+  const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    sameSite: "strict",
+  });
+
+  return token;
+};
+
+export default generateTokenAndCookie;
